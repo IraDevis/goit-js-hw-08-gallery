47fd0c5b5f24cdf5af183353fc42c773fa02cf13
Iryna Volosozharova <iradevis@gmail.com>
2021-04-18 01:15:35 +0300

    Added open/close modal functions

--- gallery.js
+++ gallery.js
@@ -1,31 +1,47 @@
-import galleryItems from './gallery-items.js'
+import galleryImages from './gallery-items.js'
 
 const refs = {
-    galleryBox: document.querySelector('.js-gallery')
-    
-}
+    galleryBox: document.querySelector('.js-gallery'),
+    lightbox: document.querySelector('.js-lightbox'),
+    modalImg: document.querySelector('.lightbox__image'),
+    modalCloseBtn: document.querySelector('.lightbox__button')
+};
 
-console.log(createImgCard(galleryItems));
+const galleryMarkup = createImgCard(galleryImages);
 
-const galleryMarkup = createImgCard(galleryItems);
+refs.galleryBox.insertAdjacentHTML('beforeend', galleryMarkup);
 
-refs.galleryBox.insertAdjacentHTML('beforeend',galleryMarkup);
+refs.galleryBox.addEventListener('click', onGalleryCardClick);
+refs.modalCloseBtn.addEventListener('click', onModalClose);
 
-function createImgCard(galleryItems) {
-    return galleryItems.map((item, index) => {
+function createImgCard(galleryImages) {
+    return galleryImages.map((image, index) => {
         return `<li class="gallery__item">
   <a
     class="gallery__link"
-    href="${item.original}"
+    href="${image.original}"
   >
     <img
       class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
     />
   </a>
 </li>`;
     }).join('');
-}; 
+};
+
+function onGalleryCardClick(e) {
+    e.preventDefault();
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    };
+    refs.lightbox.classList.add('is-open');
+};
+    
+function onModalClose() {
+    refs.lightbox.classList.remove('is-open')
+};
+
 
